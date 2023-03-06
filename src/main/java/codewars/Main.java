c6f48efb3dea9551b1eb0032b15b812c588ed5dd
Marta Liushniak <marta.liushniak@yahoo.com>
2023-03-06 17:06:51 +0200

    convert-string-to-camel-case

--- src/main/java/codewars/Main.java
+++ src/main/java/codewars/Main.java
@@ -2,6 +2,16 @@ package codewars;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(toCamelCase("The_Stealth_Warrior"));
+    }
 
+    public static String toCamelCase(String s) {
+        String[] words = s.split("[-_]");
+        StringBuilder builder = new StringBuilder(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            builder.append(words[i].substring(0, 1).toUpperCase())
+                    .append(words[i].substring(1).toLowerCase());
+        }
+        return builder.toString();
     }
 }
